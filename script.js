c5fb84b861cd5a685b116a4bd95116f4612b5bef
Eddie Espinosa <espinosa.edilson@gmail.com>
2022-10-24 15:50:42 -0400

    mores styling

---
+++ script.js
@@ -0,0 +1,20 @@
+const family = [
+    {
+        dad: true,
+        name: "Joe",
+        job: "Mechanic"
+    },
+    {
+        kid1: "Mia",
+        pet: "🐴"
+    }
+]
+console.log(family);
+
+document.getElementById('contact').style.color = "#fff000";
+
+const handleColorChange = () =>{
+    console.log("change color");
+}
+
+const showAlert = () => alert("Hi There");
